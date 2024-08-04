8e88bed010d7e2ad60a938cb6cbfb0771d103941
Jared <jared.cavalli@gmail.com>
2024-08-04 14:24:41 +1000

    spotify api

--- script.js
+++ script.js
@@ -13,6 +13,41 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 });
 
+// // Pre-generated authorization token
+// const token = 'BQAubCMhNIm_O5lgBPBjOY7_fxZxofAYiG6kD3SRJfILbOibm_Baw-VOxNDCWh2UygJydxpCYzlwwsFdv73LLdsFqb250h6Tk3_CaDDInuJd5Od9AWSZ5uX7ZsJ59fyurdsCU4hkc08shPhc4LLrIsbhXSFOtZe4MsoIlfs625WZ2lOhkSMu50BcO8O3UoAHvefkbkU-wmLntESYeVsgIYR6X2a-t4CTmLzmY8ffLmFDjTAXNsR-IfzhygbAGbgrAeYhE_9cj4Y';
+
+// async function fetchWebApi(endpoint, method, body) {
+//   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//     method,
+//     body: JSON.stringify(body)
+//   });
+//   return await res.json();
+// }
+
+// async function getTopTracks() {
+//   return (await fetchWebApi(
+//     'v1/me/top/tracks?time_range=short_term&limit=5', 'GET'
+//   )).items;
+// }
+
+// document.addEventListener('DOMContentLoaded', async function() {
+//   try {
+//     const topTracks = await getTopTracks();
+//     const topTrackInfo = topTracks?.map(
+//       ({ name, artists }) =>
+//         `${name} by ${artists.map(artist => artist.name).join(', ')}`
+//     ).join(', ');
+
+//     document.getElementById('artist').innerText = topTrackInfo || 'No top tracks found';
+//   } catch (error) {
+//     console.error('Error fetching the artist:', error);
+//     document.getElementById('artist').innerText = 'Error fetching the artist';
+//   }
+// });
+
 // Pre-generated authorization token
 const token = 'BQAubCMhNIm_O5lgBPBjOY7_fxZxofAYiG6kD3SRJfILbOibm_Baw-VOxNDCWh2UygJydxpCYzlwwsFdv73LLdsFqb250h6Tk3_CaDDInuJd5Od9AWSZ5uX7ZsJ59fyurdsCU4hkc08shPhc4LLrIsbhXSFOtZe4MsoIlfs625WZ2lOhkSMu50BcO8O3UoAHvefkbkU-wmLntESYeVsgIYR6X2a-t4CTmLzmY8ffLmFDjTAXNsR-IfzhygbAGbgrAeYhE_9cj4Y';
 
@@ -27,21 +62,18 @@ async function fetchWebApi(endpoint, method, body) {
   return await res.json();
 }
 
-async function getTopTracks() {
+async function getTopArtists() {
   return (await fetchWebApi(
-    'v1/me/top/tracks?time_range=short_term&limit=1', 'GET'
+    'v1/me/top/artists?time_range=short_term&limit=5', 'GET'
   )).items;
 }
 
 document.addEventListener('DOMContentLoaded', async function() {
   try {
-    const topTracks = await getTopTracks();
-    const topTrackInfo = topTracks?.map(
-      ({ name, artists }) =>
-        `${name} by ${artists.map(artist => artist.name).join(', ')}`
-    ).join(', ');
+    const topArtists = await getTopArtists();
+    const topArtistInfo = topArtists?.map(artist => artist.name).join(', ');
 
-    document.getElementById('artist').innerText = topTrackInfo || 'No top tracks found';
+    document.getElementById('artist').innerText = topArtistInfo || 'No top artists found';
   } catch (error) {
     console.error('Error fetching the artist:', error);
     document.getElementById('artist').innerText = 'Error fetching the artist';
